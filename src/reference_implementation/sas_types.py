f81cd09da4a1e66776649aeca161fc4cac08fd8c
nicks-mitre <nstegman@mitre.org>
2023-06-16 11:40:24 -0600

    Create sas_types

---
+++ src/reference_implementation/sas_types.py
@@ -0,0 +1,7 @@
+"""
+This module contains class definitions for objects used by SAS-CBSD interactions according to the following documents:
+WINNF-TS-0016-V1.2.7 SAS to CBSD Technical Specification
+
+
+
+"""
