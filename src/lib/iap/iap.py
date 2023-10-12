a63f39199ea32ab14970de62865276e7f3b3faa6
Jordan Graham <gjordan@mitre.org>
2023-10-12 02:04:53 -0400

    Package iap module

--- src/lib/iap/iap.py
+++ src/lib/iap/iap.py
@@ -36,10 +36,6 @@
 ==================================================================================
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from collections import namedtuple
 from functools import partial
 import logging
